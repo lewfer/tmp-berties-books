3621063538013be016b4c9c4ddb21696c6652e00
lewfer <lif@arcwave.co.uk>
2024-09-10 18:41:35 +0100

    INiti

---
+++ index.js
@@ -0,0 +1,41 @@
+// Setup express and ejs
+var express = require ('express')
+var ejs = require('ejs')
+
+var mysql = require('mysql2');
+
+// Create the express application object
+const app = express()
+const port = 8000
+
+// Tell Express that we want to use EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true })); 
+
+// Set up css
+app.use(express.static(__dirname + '/public'));
+
+// Define the database connection
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'berties_books_app',
+    password: 'qwertyuiop',
+    database: 'berties_books'
+});
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+global.db = db;
+
+// Load the route handlers
+const mainRoutes = require("./routes/main");  
+app.use('/', mainRoutes);
+
+// Start the web app listening
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
